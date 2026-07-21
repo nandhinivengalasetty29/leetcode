bea1f00eea6f64994820ed126e4f4d83bbb4c14c
nandhinivengalasetty29 <nandhinivengalasetty29@gmail.com>
2026-07-21 18:01:26 +0530

    Time: 4 ms (52.54%), Space: 14.2 MB (17.31%) - LeetHub

---
+++ 0137-single-number-ii/0137-single-number-ii.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def singleNumber(self, nums):
+        count={}
+        for num in nums:
+            if num in count:
+                count[num]+=1
+            else:
+                count[num]=1
+        for num in count:
+            if count[num]==1:
+                return num
